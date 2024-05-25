959d1d93da3a083e818bd86850a7c35fb22ee141
MeenaHaira <meenadharmalingam67@gmail.com>
2024-05-25 18:44:23 +0530

    first commit

---
+++ src/test/java/com/demo/A.java
@@ -0,0 +1,50 @@
+package com.demo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import net.bytebuddy.matcher.EqualityMatcher;
+
+public class A {
+	
+	public static void main(String[] args) throws IOException {
+//		File f = new 
+//				File("C:\\Users\\Apple\\eclipse-workspace\\Miniproject_6PM\\excel\\6PM_Sheet.xlsx");
+//		
+//		FileInputStream fis = new FileInputStream(f);
+//				
+//		Workbook wb = new XSSFWorkbook(fis);
+//		
+//		
+//		//wb.createSheet("Aasha").createRow(0).createCell(0).setCellValue("S.Name");
+//		wb.getSheet("Sheet1").createRow(4).createCell(0).setCellValue("Aasha");
+//		wb.getSheet("Sheet1").getRow(4).createCell(1).setCellValue("Meena");
+//		
+//		FileOutputStream fos = new FileOutputStream(f);
+//		wb.write(fos);
+//		wb.close();
+		
+		System.out.println("Successfully added");
+		
+		
+		
+	}
+	
+	
+	
+	
+		
+
+}
